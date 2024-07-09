885078417259a9413aac6840be26490d62824983
Sallytion <Yash.tekwani2022@vitstudent.ac.in>
2024-07-09 18:25:54 +0530

    added Order controls

--- Server/controllers/OrderController.js
+++ Server/controllers/OrderController.js
@@ -22,3 +22,52 @@ export async function AddOrder(req,res){
         }
     }
 }
+
+export async function GetOrders(req,res){
+    try {
+        const orders = await orderDB.find({});
+        if(orders){
+            res.status(200).json(orders);
+        }else{
+            res.status(404).json({message: "No orders found"});
+        }
+    } catch (error) {
+        res.status(400).json({error: error});
+    }
+}
+
+export async function GetOrderById(req,res){
+    const {id} = req.body;
+    if(!id){
+        return res.status(400).json({message: "Order Id is required"});
+    }else{
+        try {
+            const order = await orderDB.findById(id);
+            if(order){
+                res.status(200).json(order);
+            }else{
+                res.status(404).json({message: "No order found"});
+            }
+        } catch (error) {
+            res.status(400).json({error: error});
+        }
+    }
+}
+
+export async function deleteOrder(req,res){
+    const {id} = req.body;
+    if(!id){
+        return res.status(400).json({message: "Order Id is required"});
+    }else{
+        try {
+            const order = await orderDB.findByIdAndDelete(id);
+            if(order){
+                res.status(200).json({message: "Order deleted successfully"});
+            }else{
+                res.status(404).json({message: "No order found"});
+            }
+        } catch (error) {
+            res.status(400).json({error: error});
+        }
+    }
+}
--- Server/routes/OrderRoute.js
+++ Server/routes/OrderRoute.js
@@ -3,5 +3,8 @@ const router = express.Router();
 import * as OrderController from '../controllers/OrderController.js';
 
 router.post("/AddOrder", OrderController.AddOrder);
+router.post("/GetOrders", OrderController.GetOrders);
+router.post("/GetOrderById", OrderController.GetOrderById);
+router.post("/deleteOrder", OrderController.deleteOrder);
 
 export default router;
